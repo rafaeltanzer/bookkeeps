2c712be6b2cbf86aa4784f9794e9e49a979bb7f9
rafaeltanzer <rafael.tanzer02@gmail.com>
2022-04-04 15:04:48 +0200

    databaseMIddleware done, hopefully

--- server/ts/src/services/database_logic/databaseMiddleware.ts
+++ server/ts/src/services/database_logic/databaseMiddleware.ts
@@ -1,17 +1,32 @@
 import * as models from '../../models/allModels';
 import * as argon2 from 'argon2';
-import {model, Error} from 'mongoose';
 import { matchingActualAmount } from './databaseValidation';
 import mongoose from 'mongoose';
+import { BaseModelController } from './databaseController';
 //create() fires save() hook!
 
 //#region User-Hooks
 models.UserSchema.pre('save', async function(){
+    console.log("PreSave in User: " + this + "Type =" + typeof this);
+
     this.pswd = await argon2.hash(this.pswd);
 });
+
+
+//https://www.tabnine.com/code/javascript/functions/mongoose/Schema/post
+models.UserSchema.post('deleteOne', async function(){
+    console.log("Post delete User middleware..."+ this + "Type = " + typeof this);
+    //let userEMail = this.email maybe this refers to the query and not the document
+    const dbController = new BaseModelController<models.Account>(models.AccountSchema, "Account");
+    await dbController.deleteOneDoc({userMail: this.email});
+});
 //#endregion
+
+
 //#region Account-Hooks
 models.AccountSchema.pre(['save', 'updateOne'], {document: true, query: false}, async function(){
+    console.log("Pre Save,Update Account middleware..."+ this + "Type = " + typeof this);
+
     if(!(await matchingActualAmount(this.incomingAmount, this.outgoingAmount, this.actualAmount)))
     {
         throw new mongoose.Error.ValidatorError({
@@ -21,8 +36,11 @@ models.AccountSchema.pre(['save', 'updateOne'], {document: true, query: false}, 
         });
     }
 });
-//#endregion
 
-export const UserModel = model<models.User>("User", models.UserSchema);
-export const TransactionModel = model<models.Transaction>('Transaction', models.TransactionSchema);
-export const AccountModel = model<models.Account>('Account', models.AccountSchema);
+models.AccountSchema.post('deleteOne', async function(){
+    console.log("INside Post AccountDelete" +this);
+
+    const dbController = new BaseModelController<models.User>(models.UserSchema,"User");
+    await dbController.deleteOneDoc({accountName: this.name});
+});
+//#endregion
